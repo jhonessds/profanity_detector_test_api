47374e13ed3e3801f68f08d5ddb8477f5cee5324
Jhones Silva dos Santos <jhonessds@gmail.com>
2020-12-01 10:23:01 -0300

    metodo para add words

---
+++ ProfanityDetector/BadWord.cs
@@ -0,0 +1,14 @@
+﻿namespace ProfanityDetector
+{
+    public class BadWord
+    {
+        public BadWord(string word, string language)
+        {
+            Word = word;
+            Language = language;
+        }
+
+        public string Word { get; set; }
+        public string Language { get; set; }
+    }
+}
